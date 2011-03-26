792d580d68231c9b4b8bb841f47a9ab456e4fb0e
Davy Campano <davy@spicerackmedia.com>
2011-03-26 09:09:47 -0500

    refactoring the way Fog gets accessed

--- lib/sr-scripts.rb
+++ lib/sr-scripts.rb
@@ -1,5 +1,20 @@
-module Sr
-  module Scripts
-    # Your code goes here...
-  end
+require 'yaml'
+
+module SrScripts
+	class Compute 
+		def self.get 
+			yml = YAML.load_file '/etc/sr-scripts.yml'	
+			@aws_access_key = yml["aws_access_key"]
+			@aws_secret_key = yml["aws_secret_key"]	
+			return Fog::Compute.new(:provider => "AWS", :aws_access_key_id => @aws_access_key, :aws_secret_access_key => @aws_secret_key, :region => "us-west-1")
+		end
+	end
+	class SimpleDB
+		def self.get
+			yml = YAML.load_file '/etc/sr-scripts.yml'	
+			@aws_access_key = yml["aws_access_key"]
+			@aws_secret_key = yml["aws_secret_key"]	
+			return Fog::AWS::SimpleDB.new(:aws_access_key_id => @aws_access_key, :aws_secret_access_key => @aws_secret_key)
+		end
+	end
 end
